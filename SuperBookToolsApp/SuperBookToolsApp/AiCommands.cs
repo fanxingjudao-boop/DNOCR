fe8fdf40dd944cb951113b9126f45e28f8725d4d
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2026-01-18 15:39:27 +0900

    途中さん

---
+++ SuperBookToolsApp/SuperBookToolsApp/AiCommands.cs
@@ -0,0 +1,130 @@
+﻿#pragma warning disable CA2235 // Mark all non-serializable fields
+
+using System;
+using System.Buffers;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+using IPA.Cores.Basic;
+using IPA.Cores.Helper.Basic;
+using static IPA.Cores.Globals.Basic;
+
+using IPA.Cores.Codes;
+using IPA.Cores.Helper.Codes;
+using static IPA.Cores.Globals.Codes;
+
+using SuperBookTools;
+using SuperBookTools.App;
+
+namespace SuperBookTools.App
+{
+    public static class SuperBookExternalTools
+    {
+        public static readonly ImageMagickUtil ImageMagick = new ImageMagickUtil(new ImageMagickOptions(
+            Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\image_tools\ImageMagick-7.1.1-47-portable-Q16-HDRI-x64\magick.exe"),
+            Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\image_tools\ImageMagick-7.1.1-47-portable-Q16-HDRI-x64\mogrify.exe"),
+            Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\image_tools\exiftool-13.30_64\exiftool.exe"),
+            Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\image_tools\QPDF\bin\qpdf.exe"),
+            Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\image_tools\pdfcpu\pdfcpu_0.11.0_Windows_x86_64\pdfcpu.exe")
+        ));
+
+        public static readonly FfMpegUtil FfMpeg = new FfMpegUtil(new FfMpegUtilOptions(
+            Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\ffmpeg\240703\ffmpeg.exe"),
+            Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\ffmpeg\240703\ffprobe.exe")));
+
+        public static readonly AiUtilBasicSettings Settings = new AiUtilBasicSettings
+        {
+            AiTest_RealEsrgan_BaseDir = Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\image_tools\RealEsrgan\RealEsrgan_Repo"),
+            AiTest_TesseractOCR_Data_Dir = Path.Combine(Env.AppRootDir, @"..\external_tools\external_tools\image_tools\TesseractOCR_Data\250614\tessdata"),
+        };
+        public static readonly AiTask Task = new AiTask(Settings, FfMpeg);
+
+        public static AiRandomBgmSettingsFactory SettingsFactory = null!;
+    }
+
+    public static partial class Commands
+    {
+        [ConsoleCommand(
+            "AiPdfSuper command",
+            "AiPdfSuper",
+            "AiPdfSuper command")]
+        public static async Task<int> AiPdfSuper(ConsoleService c, string cmdName, string str)
+        {
+            ConsoleParam[] args =
+            {
+            };
+            ConsoleParamValueList vl = c.ParseCommandList(cmdName, str, args);
+
+            string srcDir = @"C:\tmp\260118pdftest\src";
+            string dstDir = @"C:\tmp\260118pdftest\dst";
+
+            SuperPerformPdfOptions options = new SuperPerformPdfOptions {/* MaxPagesForDebug = 120, SaveDebugPng = true, SkipRealesrgan = true */ };
+
+            var srcFiles = (await Lfs.EnumDirectoryAsync(srcDir, true)).Where(x => x.IsFile && x.Name.StartsWith("_") == false && x.Name._IsExtensionMatch(".pdf")).OrderBy(x => x.FullPath, StrCmpi)._Shuffle().ToList();
+
+            int numTotal = srcFiles.Count();
+            int numOk = 0;
+            int numError = 0;
+            int numSkip = 0;
+
+            $"Total {numTotal} Files"._Error();
+
+            int currentNumber = 0;
+
+            List<string> errorFilesList = new();
+
+            foreach (var src in srcFiles)
+            {
+                currentNumber++;
+                string relativePath = PP.GetRelativeFileName(src.FullPath, srcDir);
+                string dstPath = PP.Combine(dstDir, relativePath);
+
+                $"<< {currentNumber} / {numTotal} >> '{src.FullPath}' Start"._Error();
+
+                try
+                {
+                    if (await SuperPdfUtil.PerformPdfAsync(src.FullPath, dstPath, options) == false)
+                    {
+                        numSkip++;
+                        $"<< {currentNumber} / {numTotal} >> '{src.FullPath}' Skip"._Error();
+                    }
+                    else
+                    {
+                        numOk++;
+                        $"<< {currentNumber} / {numTotal} >> '{src.FullPath}' OK"._Error();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Con.WriteLine($"<< {currentNumber} / {numTotal} >> Error: {src.FullPath} -> {dstPath}");
+                    ex._Error();
+                    errorFilesList.Add(src.FullPath);
+                    numError++;
+                }
+            }
+
+            if (errorFilesList.Count >= 1)
+            {
+                $"--- Error files ---"._Error();
+                foreach (var errFile in errorFilesList)
+                {
+                    $"- {errFile}"._Error();
+                }
+            }
+
+            $"\n\n<< AiPdfSuper Result >>\nnumTotal = {numTotal}, numSkip = {numSkip}, numOk = {numOk}, numError = {numError}\n\n"._Error();
+
+            return 0;
+        }
+    }
+}
